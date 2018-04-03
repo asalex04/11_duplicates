0247763a93fcd50c67f040e731a182e5258cf71e
asalex04 <arc04@mail.ru>
2018-04-03 10:51:29 +0400

    changed variable names and removed loop

--- duplicates.py
+++ duplicates.py
@@ -15,45 +15,40 @@ def get_parser():
 
 def get_duplicates(dir_path):
     filenames_with_pathes_dict = collections.defaultdict(list)
-    for root, dirs, files in os.walk(dir_path):
-        for filename in files:
+    for root, dirs, file_names in os.walk(dir_path):
+        for filename in file_names:
             fullpath = os.path.join(root, filename)
             file_hash = get_file_hash_md5(fullpath)
             filenames_with_pathes_dict[file_hash].append(fullpath)
-    return files_with_pathes_dict
+    return filenames_with_pathes_dict
 
 
-def get_file_hash_md5(file_for_hash, first_block=False):
+def get_file_hash_md5(file_for_hash):
     hasher = hashlib.md5()
     blocksize = 1024 * 1024
-    file_size = os.path.getsize(file_for_hash)
     with open(file_for_hash, 'rb') as file:
-        while True:
-            buff = file.read(blocksize)
-            hasher.update(buff)
-            file_size -= blocksize
-            if (len(buff) < blocksize or first_block):
-                break
-        return hasher.hexdigest()
+        buff = file.read(blocksize)
+        hasher.update(buff)
+    return hasher.hexdigest()
 
 
-def print_path_duplicate_file(print_dir_path):
-    print('Scanning... {}'.format(print_dir_path))
-    for _, list_of_filepaths in files_hash_and_path_dict.items():
-        if len(list_of_filepaths) > 1:
+def print_path_duplicate_file(files_duplicates):
+    print('Scanning... {}'.format(files_duplicates))
+    for _, list_of_duplicates in files_hash_and_path_dict.items():
+        if len(list_of_duplicates) > 1:
             print('\nDuplicates:')
-            print('\n'.join(list_of_filepaths))
+            print('\n'.join(list_of_duplicates))
 
 
 if __name__ == '__main__':
     parser = get_parser()
-    search_dir_path = parser.parse_args().path
+    list_of_filepaths = parser.parse_args().path
 
-    if os.path.isdir(search_dir_path):
-        files_hash_and_path_dict = get_duplicates(search_dir_path)
+    if os.path.isdir(list_of_filepaths):
+        files_hash_and_path_dict = get_duplicates(list_of_filepaths)
         if files_hash_and_path_dict:
-            print_path_duplicate_file(search_dir_path)
+            print_path_duplicate_file(list_of_filepaths)
         else:
-            print('Nothing found in {}'.format(search_dir_path))
+            print('Nothing found in {}'.format(list_of_filepaths))
     else:
         print('Enter existing directory')
